9613a2f4be3c9261f4c56538fc689106f021c4c6
Massive12356 <edwardmintah17@gmail.com>
2025-08-01 15:27:47 +0000

    removed Alices work

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import JoinUs from "../components/ForCareersPage/JoinUs";
-import CoreValues from "../components/ForCareersPage/CoreValues";
-import Impressions from "../components/ForCareersPage/Impressions";
-import TeamTestimonial from "../components/ForCareersPage/TeamTestimonial";
+
 
 const Careers = () => {
   return (
     <div>
-      <JoinUs />
-      <CoreValues />
-      <Impressions />
-      <TeamTestimonial />
+     
     </div>
   );
 };
