01866e76059871b9976e2582bafb01be6862b1fc
Massive12356 <edwardmintah17@gmail.com>
2025-08-01 15:22:02 +0000

    changes made

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import JoinUs from "../components/ForCareersPage/JoinUs";
+import CoreValues from "../components/ForCareersPage/CoreValues";
+import Impressions from "../components/ForCareersPage/Impressions";
+import TeamTestimonial from "../components/ForCareersPage/TeamTestimonial";
 
 const Careers = () => {
   return (
-    <div>Careers</div>
-  )
-}
+    <div>
+      <JoinUs />
+      <CoreValues />
+      <Impressions />
+      <TeamTestimonial />
+    </div>
+  );
+};
 
-export default Careers
+export default Careers;
